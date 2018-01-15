596b42a0822e1ac89a04108c53dfbe34dd76da87
RajarajanNagarajan <35371171+RajarajanNagarajan@users.noreply.github.com>
2018-01-15 16:29:29 -0400

    Lab 4 - Task 1: write a function called nested_sum that takes a nested list of integers and add up the elements from all the nested lists.

--- lab4_task1.py
+++ lab4_task1.py
@@ -1,4 +1,13 @@
-a = [1,2,3,4,5]
+a = [1,[2,3,4],5,[6,7,8],9]
 
-for item in a:
-	sum = sum + item
+
+def nested_sum(x):
+    sum = 0
+    for i in x:
+        if isinstance(i,int):
+            sum += i
+        elif isinstance(i,list):
+            sum+= nested_sum(i)
+    return sum
+
+print (nested_sum(a))
