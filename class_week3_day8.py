e28dd07f0afe4d9a9b16217d1cd133f8810720f0
rajarajann <rajarajan.nagarajan@dal.ca>
2018-01-24 10:33:45 -0400

    Inheritance

---
+++ class_week3_day8.py
@@ -0,0 +1,21 @@
+class point:
+    """this is a point class"""
+    def __init__(self,x,y):
+        """constructor for point"""
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return ("The point is" + str (self.x) + "," + str (self.y))
+    def __add__(self,other):
+        my_point = point (self.x+other.x,self.y+other.y)
+        return (my_point)
+
+
+p1 = point (5,2)
+p2 = point (10,10)
+p3 = p1 + p2
+
+print (p3)
+
+
+
