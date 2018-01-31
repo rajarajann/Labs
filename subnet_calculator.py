d0e0a358b3635ecf539eee0c93060e92929f64d8
rajarajann <rajarajan.nagarajan@dal.ca>
2018-01-31 09:37:03 -0400

    start of subnet_calculator

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -1,1 +1,26 @@
 import math
+
+input_ip = input ("Enter the IP address :")
+input_smask = input ("Enter the subnet mask :")
+
+ip = input_ip.split(".")
+print (ip)
+smask = int(input_smask)
+print (type(smask))
+
+class ip_v4():
+    def __init__(self,a =0,b=0,c=0,d=0):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+    def __str__ (self):
+        return "%.3d .%.3d .%.3d .%.3d" % (self.a,self.b,self.c,self.d)
+
+
+x = ip_v4()
+print (x)
+
+
+
+
