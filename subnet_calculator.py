b0db70ecdb968b4504bd1358546f2c67068b270e
rajarajann <rajarajan.nagarajan@dal.ca>
2018-01-31 10:35:59 -0400

    binary ip

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -6,18 +6,31 @@ input_smask = input ("Enter the subnet mask :")
 ip_str = input_ip.split(".")
 print (ip_str)
 smask = int(input_smask)
-ip = list(map(int,ip_str))
+ip_int = list(map(int,ip_str))
+print (ip_int)
 
-print (ip)
+ip_bin = []
+for i in range (4):
+    ip_bin.append(bin(ip_int[i]))
+    print (ip_bin)
+
+ip_b = list(map(int,ip_bin))
 
 class ip_v4():
-    def __init__(self,a,b,c,d):
+    def __init__(self,a = ip_b[0],b =ip_b[1],c =ip_b[2],d =ip_b[3]):
         self.a = a
         self.b = b
         self.c = c
         self.d = d
+        self.d = d
     def __str__ (self):
-        return "%.3d .%.3d .%.3d .%.3d" % (self.a,self.b,self.c,self.d)
+        return "%d .%d .%d .%d" % (self.a,self.b,self.c,self.d)
+
+
+
+i = ip_v4()
+print (i)
+
 
 print (type(ip[2]))
 
