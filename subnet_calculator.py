9f894b5dd93ec773db61a1ff03a4df6cf5ea58bb
rajarajann <rajarajan.nagarajan@dal.ca>
2018-02-04 21:48:12 -0400

    wildcard in binary not showing up properly

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -3,44 +3,8 @@ import struct
 import pprint
 
 input_ip = "10.10.10.10"
-input_smask = "16"
+input_smask = "24"
 smask = int(input_smask)
-
-ip_str = input_ip.split(".") #ip is in string - ['192', '168', '1', '1']
-print(ip_str)
-ip_int = list(map(int,ip_str)) #ip is in int list - [192, 168, 1, 1]
-print(ip_int)
-
-ip_bstr = ["{0:08b}".format(ip_int[0]),"{0:08b}".format(ip_int[1]),"{0:08b}".format(ip_int[2]),"{0:08b}".format(ip_int[3])]
-print (ip_bstr) #ip is in binary string ['11000000', '10101000', '00000001', '00000001']
-ip_blist = list(map(int,ip_bstr)) #ip in binary string
-print (ip_blist) #ip is in binary int list [11000000, 10101000, 1, 1]
-
-def s_mask(smask): #got this logic from http://code.activestate.com/recipes/576483-convert-subnetmask-from-cidr-notation-to-dotdecima/
-    bits = 0
-    for i in range(32-smask,32):
-        bits |= (1 << i)
-    return "%d.%d.%d.%d" % ((bits & 0xff000000) >> 24, (bits & 0xff0000) >> 16, (bits & 0xff00) >> 8 , (bits & 0xff))
-
-subnet_str = s_mask(smask).split(".") #subnet is in string ['255', '255', '255', '0']
-print(subnet_str)
-subnet_intlist = list(map(int,subnet_str)) #subnet is in int list [255, 255, 255, 0]
-print(subnet_intlist)
-subnet_binarystr = ["{0:08b}".format(subnet_intlist[0]),"{0:08b}".format(subnet_intlist[1]),"{0:08b}".format(subnet_intlist[2]),"{0:08b}".format(subnet_intlist[3])]
-print(subnet_binarystr) #subnet is in binary string
-subnet_binarylist = list(map(int,subnet_binarystr))
-print((subnet_binarylist))
-
-def wildcard(subnet_intlist):
-    octet_list = []
-    for octet in subnet_intlist:
-        each_octet = "{0:08b}".format(255-int(octet))
-        octet_list.append(each_octet)
-    return (octet_list)
-wildcard_binarystr = wildcard(subnet_intlist)
-print (wildcard_binarystr)
-wildcard_binarylist = list(map(int,wildcard_binarystr))
-print((wildcard_binarylist))
 
 class ip_v4():
     def __init__(self,a,b,c,d):
@@ -52,7 +16,7 @@ class ip_v4():
     def __str__ (self):
         return "%d.%d.%d.%d" % (self.a,self.b,self.c,self.d)
 
-def bin_to_int(binary_str):
+def bin_to_int(binary_str): #['11111111', '11111111', '00000000', '00000000'] > 255.255.0.0
     dict = {}
     i = 0
     for each_element in binary_str: #its a list only
@@ -68,16 +32,70 @@ def bin_to_int(binary_str):
     value_list = list(dict.values())
     #print (value_list)
     ip_in_int = ip_v4(value_list[0],value_list[1],value_list[2],value_list[3])
-    print(ip_in_int)
+    return(ip_in_int)
 
-ip_binaryaddress = ip_v4(ip_blist[0],ip_blist[1],ip_blist[2],ip_blist[3])
-ip_intaddress = ip_v4(ip_int[0],ip_int[1],ip_int[2],ip_int[3])
-subnet_binaryaddress = ip_v4(subnet_binarylist[0],subnet_binarylist[1],subnet_binarylist[2],subnet_binarylist[3])
-subnet_intaddress = ip_v4(subnet_intlist[0],subnet_intlist[1],subnet_intlist[2],subnet_intlist[3])
+def int_to_ip(int_list): #['255','255',0,0] > 255.255.0.0
+    dict = {}
+    i = 0
+    for element in int_list:
+        dict[i] = element
+        i += 1
+    value_list = list(dict.values())
+    in_ip_format = ip_v4(int(value_list[0]),int(value_list[1]),int(value_list[2]),int(value_list[3]))
+    return (in_ip_format)
 
-print ("IP Address:",ip_intaddress,"       ",ip_binaryaddress)
-print ("Sub Net:",subnet_intaddress,"      ",subnet_binaryaddress)
-bin_to_int(['00000000','11111111','11111111','11111111'])
+def integer_to_bin(int_argument): #255 > 11111111
+    binary = "{0:0b}".format(int(int_argument))
+    return binary
+
+def intip_to_binip(int_list): #'255','255','0','0' > 11111111.11111111.0.0
+    dict = {}
+    i = 0
+    for i in range (len(int_list)):
+        dict [i] = integer_to_bin(int_list[i])
+        i += 1
+    value_list = list(dict.values())
+    in_ip_format = ip_v4(int(value_list[0]),int(value_list[1]),int(value_list[2]),int(value_list[3]))
+    return (in_ip_format)
+
+def s_mask(smask): #got this logic from http://code.activestate.com/recipes/576483-convert-subnetmask-from-cidr-notation-to-dotdecima/
+    bits = 0
+    for i in range(32-smask,32):
+        bits |= (1 << i)
+    return "%d.%d.%d.%d" % ((bits & 0xff000000) >> 24, (bits & 0xff0000) >> 16, (bits & 0xff00) >> 8 , (bits & 0xff))
+
+def wildcard(subnet_intlist):
+    octet_list = []
+    for octet in subnet_intlist:
+        each_octet = "{0:08b}".format(255-int(octet))
+        octet_list.append(each_octet)
+    return (octet_list)
+
+int_ip_list_str = input_ip.split(".") #ip is in string - ['192', '168', '1', '1']
+ip_int = int_to_ip(int_ip_list_str)
+ip_bin = intip_to_binip(int_ip_list_str)
+
+
+int_subnet_list_str = s_mask(smask).split(".") #['255', '255', '255', '0']
+subnet_int = int_to_ip(int_subnet_list_str) #255.255.255.0
+subnet_bin = intip_to_binip(int_subnet_list_str) #11111111.11111111.11111111.0
+
+
+wildcard_int_ip = bin_to_int (wildcard(int_subnet_list_str))
+wildcard_bin = intip_to_binip(wildcard(int_subnet_list_str))
+"""
+wildcard_binarystr = wildcard(subnet_intlist)
+print (wildcard_binarystr)
+wildcard_binarylist = list(map(int,wildcard_binarystr))
+print((wildcard_binarylist))
+"""
+
+
+print ("IP Address:",ip_int,"       ",ip_bin)
+print ("Sub Net:",subnet_int,"      ",subnet_bin)
+print ("Wildcard:",wildcard_int_ip,"    ",wildcard_bin)#wildcard_binarystr = wildcard(int_subnet_list_str)
+#print (wildcard_binarystr)
+
 
 
 
